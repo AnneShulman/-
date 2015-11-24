9619baaa6c46d1e1552914625676fce991540e8f
chenzihao1011 <1067364769@qq.com>
2015-11-24 13:22:46 +0800

    增加部门鼠标悬浮样式

--- js/index.js
+++ js/index.js
@@ -5,4 +5,11 @@ $(function(){
 	$(".menu-btn").on('click',function(){
 		$(".strunk-menu").slideToggle("slow");
 	});
+	$(".branch").hover(function(){
+			$(this).find("h4").animate({bottom:215},500);
+			$(this).find("p").animate({top:35},500);
+		},function(){
+			$(this).find("h4").animate({bottom:0},300);
+			$(this).find("p").animate({top:250},300);
+	});
 })
