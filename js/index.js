4ea83a29ff15cd519e2bac8fe39a538a1c9ddd84
chenzihao1011 <1067364769@qq.com>
2015-11-25 12:48:52 +0800

    调部门bug

--- js/index.js
+++ js/index.js
@@ -40,9 +40,8 @@ $(function(){
 		$(".strunk-menu").slideToggle("slow");
 	});
 	
-	var ht;
+	var ht=$(".branch h4").css("top");
 	$(".branch").hover(function(){
-			ht=$(".branch h4").css("top");
 			$(this).find("h4").animate({top:0},500);
 			$(this).find("p").animate({top:39},500);
 		},function(){
@@ -50,4 +49,21 @@ $(function(){
 			$(this).find("h4").animate({top:ht},300);
 			$(this).find("p").animate({top:ht2},300);
 	});
+	// 更改页面大小时触发
+	$(window).resize(function(){
+   		var height=$(".branch").css("height");
+   		if(height=="250px"){
+		   	$(".branch h4").css("top",215);
+		   	$(".branch p").css("top",250);
+		}else if(height=="210px"){
+		   	$(".branch h4").css("top",175);
+		}else if(height=="170px"){
+		    $(".branch h4").css("top",135);
+		}else if(height=="120px"){
+		    $(".branch h4").css("top",85);
+		}
+		ht=$(".branch h4").css("top");
+	});
 })
+
+
